a0260008844e1992bcee96837289fbbb477315de
bhaskarn sharma <imbhaskaran@gmail.com>
2023-01-01 00:13:51 +0530

    store

---
+++ .keystone/config.js
@@ -0,0 +1,130 @@
+"use strict";
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// keystone.ts
+var keystone_exports = {};
+__export(keystone_exports, {
+  default: () => keystone_default
+});
+module.exports = __toCommonJS(keystone_exports);
+var import_core2 = require("@keystone-6/core");
+
+// schemas/schema.ts
+var import_core = require("@keystone-6/core");
+var import_access = require("@keystone-6/core/access");
+var import_fields = require("@keystone-6/core/fields");
+var lists = {
+  User: (0, import_core.list)({
+    access: import_access.allowAll,
+    fields: {
+      firstName: (0, import_fields.text)({ validation: { isRequired: true } }),
+      lastName: (0, import_fields.text)({ validation: { isRequired: true } }),
+      email: (0, import_fields.text)({
+        validation: { isRequired: true },
+        isIndexed: "unique"
+      }),
+      mobile: (0, import_fields.text)({
+        validation: { isRequired: true },
+        isIndexed: "unique"
+      }),
+      password: (0, import_fields.password)({ validation: { isRequired: true } }),
+      createdAt: (0, import_fields.timestamp)({
+        defaultValue: { kind: "now" }
+      })
+    }
+  }),
+  Product: (0, import_core.list)({
+    access: import_access.allowAll,
+    fields: {
+      title: (0, import_fields.text)({ validation: { isRequired: true } }),
+      description: (0, import_fields.text)({ validation: { isRequired: true } }),
+      image: (0, import_fields.image)({ storage: "my_local_images" }),
+      tags: (0, import_fields.relationship)({
+        ref: "Tag.products",
+        many: true,
+        ui: {
+          displayMode: "cards",
+          cardFields: ["name"],
+          inlineEdit: { fields: ["name"] },
+          linkToItem: true,
+          inlineConnect: true,
+          inlineCreate: { fields: ["name"] }
+        }
+      })
+    }
+  }),
+  Tag: (0, import_core.list)({
+    access: import_access.allowAll,
+    ui: {
+      isHidden: true
+    },
+    fields: {
+      name: (0, import_fields.text)(),
+      products: (0, import_fields.relationship)({ ref: "Product.tags", many: true })
+    }
+  })
+};
+
+// auth.ts
+var import_crypto = require("crypto");
+var import_auth = require("@keystone-6/auth");
+var import_session = require("@keystone-6/core/session");
+var sessionSecret = process.env.SESSION_SECRET;
+if (!sessionSecret && process.env.NODE_ENV !== "production") {
+  sessionSecret = (0, import_crypto.randomBytes)(32).toString("hex");
+}
+var { withAuth } = (0, import_auth.createAuth)({
+  listKey: "User",
+  identityField: "email",
+  sessionData: "firstName lastName createdAt",
+  secretField: "password",
+  initFirstItem: {
+    fields: ["firstName", "lastName", "email", "mobile", "password"]
+  }
+});
+var sessionMaxAge = 60 * 60 * 24 * 30;
+var session = (0, import_session.statelessSessions)({
+  maxAge: sessionMaxAge,
+  secret: sessionSecret
+});
+
+// keystone.ts
+var keystone_default = withAuth(
+  (0, import_core2.config)({
+    db: {
+      provider: "sqlite",
+      url: "file:./keystone.db"
+    },
+    storage: {
+      my_local_images: {
+        kind: "local",
+        type: "image",
+        generateUrl: (path) => `/images${path}`,
+        serverRoute: {
+          path: "/images"
+        },
+        storagePath: "public/images"
+      }
+    },
+    lists,
+    session
+  })
+);
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {});
